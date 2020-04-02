725dcbd43dbea069753b16d75b8d57a2eac0cb06
jameshih <j@shih.app>
2020-04-02 18:22:02 -0400

    fixed ipfs and gh-page deploymeny issue

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # ETH Colors
 
-### Translating ETH address into a spectrum of colors 
+### Translating ETH address into a spectrum of colors
 
 ![img](https://github.com/jameshih/ethcolors/blob/master/screen.png?raw=true)
 
@@ -8,3 +8,9 @@
 ```
 $ npm install
 $ npm start
+```
+
+### Deployment
+
+- gh-page [jameshih.com/ethcolors](https://jameshih.com/ethcolors)
+- ipfs [QmeLgC1iWveukEQQ8CBV2ETUKL2NZ4eVwvPdM9zTDVvaJb](http://127.0.0.1:8080/ipfs/QmeLgC1iWveukEQQ8CBV2ETUKL2NZ4eVwvPdM9zTDVvaJb/)
--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React, { lazy, Suspense } from "react";
-import { BrowserRouter, Route } from "react-router-dom";
-import Loading from "./Loading";
-const Home = lazy(() => import("./Home"));
+//import { Route, BrowserRouter, Switch } from "react-router-dom";
+//import IpfsRouter from "ipfs-react-router";
+//import Loading from "./Loading";
+//const Home = lazy(() => import("./Home"));
+import Home from "./Home";
 
 const App = () => {
   return (
     <div>
-      <BrowserRouter>
-        <Suspense fallback={<Loading />}>
-          <Route path="/" exact component={Home}></Route>
-        </Suspense>
-      </BrowserRouter>
+      {
+        //<BrowserRouter basename={process.env.PUBLIC_URL}>
+        //<Switch>
+        //<Suspense fallback={<Loading />}>
+        //<Route path="/" exact component={Home}></Route>
+        //</Suspense>
+        //</Switch>
+        //</BrowserRouter>
+      }
+      <Home />
     </div>
   );
 };
